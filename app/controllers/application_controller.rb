384a988eb0c3c7dbc544b73a3ac1604353fe1721
ren.uetake <ren.of.bamboo616@gmail.com>
2021-01-26 23:40:30 +0000

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  
+  private
+  
+  def require_user_loggied_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
